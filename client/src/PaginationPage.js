72feb59fc38cb24154be79dc20bd8a2ed36f3365
Abhinav1190P <pandeysandeep1190@gmail.com>
2024-03-31 18:58:33 +0530

    Some fixes

--- client/src/PaginationPage.js
+++ client/src/PaginationPage.js
@@ -37,7 +37,7 @@ function PaginationPage() {
     return (
         <Box w="100vw" h={'100vh'}>
             <Flex alignItems={'center'} justifyContent={'center'} w="100%" h="100%">
-                <VStack spacing={4} align="stretch" maxW="700px" w="100%" p={4}>
+                <VStack spacing={4} align="stretch" maxW="900px" w="100%" p={4}>
                     {loading ? (
                         <Text>Loading...</Text>
                     ) : error ? (
@@ -47,9 +47,13 @@ function PaginationPage() {
                             <Text fontSize="xl" fontWeight="bold">Users Page {page || 1}</Text>
                             <Flex flexWrap="wrap" justify="space-between">
                                 {users.map((user) => (
-                                    <Flex align="center" justify="flex-start" bg="white" p={4} borderRadius="md" boxShadow="md" m={2}>
+                                    <Flex align="center" cursor={'pointer'} justify="flex-start" bg="white" p={4} borderRadius="md" boxShadow="lg" m={2}>
                                         <Avatar src={user.avatar} alt={`${user.first_name} ${user.last_name}`} size="md" mr={4} />
-                                        
+                                        <Box>
+                                            <Text fontSize="12px" fontWeight="bold">{user.first_name} {user.last_name}</Text>
+                                            <Text fontSize="sm" color="gray.600">Available: {user.available ? 'Yes' : 'No'}</Text>
+
+                                        </Box>
                                     </Flex>
                                 ))}
                             </Flex>
